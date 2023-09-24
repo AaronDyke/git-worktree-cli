f8bb07932ac52a22813d24d6363d251c0ff04254
Aaron Dyke <54460524+AaronDyke@users.noreply.github.com>
2023-09-23 23:18:57 -0400

    update open docs

--- cmd/open.go
+++ cmd/open.go
@@ -13,13 +13,8 @@ import (
 // openCmd represents the open command
 var openCmd = &cobra.Command{
 	Use:   "open",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Open worktree in VSCode",
+	Long:  `Open worktree in VSCode`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("open called")
 
